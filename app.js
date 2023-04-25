f73343de97a42d3f8424281b6731c8298aaf0c57
SamuelForestier <samuelforestier.etu@gmail.com>
2023-04-25 10:54:15 +0200

    Adding middleware

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 const productsRoutes = require('./shop_api/routes/products');
 const ordersRoutes = require('./shop_api/routes/orders');
+
+app.use(morgan('dev'));
 
 app.use('/products', productsRoutes);
 app.use('/orders', ordersRoutes);
